cfa57dfdaa1adf1811ebedd4a9d3f06613ac2184
Jo_Na_Than <mmadujonathan@gmail.com>
2022-06-22 23:42:19 +0100

    Updated a message to the Mentors and the README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JavaScript Print Output
  A Javascript code that prints out my name, height, and country on the screen
+ This is the link to my github page: https://jozz77.github.io/JavaScript-Print-Output/
--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
+let myMentorMessage = 'Hi Mentors, I am very sorry to share the link to my Github page instead of the link to my repository. Here is the repository link: https://github.com/Jozz77/JavaScript-Print-Output.  Thanks for understanding. Click Ok to view the assignment';
 let myName = 'Hey there, I am Jo_Na_Than';
 let myHeight = 'I am 5.11ft tall...maybe not too tall enough but I think I am a little bit above average height';
 let myCountry = 'I live in Nigeria where Sapa wants to finish someone';
-let myContinue = 'Refresh to get my details once again';
 
+alert(myMentorMessage)
 alert(myName);
 alert(myHeight);
 alert(myCountry);
