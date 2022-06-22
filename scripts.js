247e0b922c65ddb950a0068d7ccebf5e1c855862
Jo_Na_Than <mmadujonathan@gmail.com>
2022-06-22 10:33:57 +0100

    just created

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var firstName = "Jonathan";
+var lastName = "Mmadu";
+var middleName = "Onyedikachi";
+var myHeight = "5.11ft";
+var myCountry = "Nigeria";
+
+myNames = firstName + " " + lastName + " " + middleName;
+
+document.write('Name: ' + myNames + '<br/>');
+document.write('Height: ' + myHeight + '<br/>');
+document.write('Country: ' + myCountry + '<br/>' + '<br/>');
+document.write('I am ' + myNames + ', I am nearly ' + myHeight + ' tall and I come from ' + myCountry + ' where Sapa want to finish somebody.' )
