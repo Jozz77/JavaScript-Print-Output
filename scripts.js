0a5538d81726daedd7b8ca2b01ed4b3abdd818a4
Jo_Na_Than <mmadujonathan@gmail.com>
2022-06-26 16:20:55 +0100

    updated

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let myMentorMessage = 'Hi Mentors, I am very sorry to share the link to my Github page instead of the link to my repository. Here is the repository link: https://github.com/Jozz77/JavaScript-Print-Output.  Thanks for understanding. Click Ok to view the assignment';
+let myMentorMessage = 'Hi Mentors, I am very sorry to share the link to my Github page instead of the link to my repository. The repository link will appear the operation has been completed. Thanks for understanding. Click Ok to view the assignment';
 let myName = 'Hey there, I am Jo_Na_Than';
 let myHeight = 'I am 5.11ft tall...maybe not too tall enough but I think I am a little bit above average height';
 let myCountry = 'I live in Nigeria where Sapa wants to finish someone';
@@ -7,4 +7,3 @@ alert(myMentorMessage)
 alert(myName);
 alert(myHeight);
 alert(myCountry);
-alert(myContinue)
